ab6b63dbdfb44c1acd59fa4dd4cb83d00e1a486a
bowenyou <bowenyou@users.noreply.github.com>
2024-11-25 06:16:21 -0500

    fix gamegit add .git add . and add tests

--- contracts/src/interfaces/IGame.sol
+++ contracts/src/interfaces/IGame.sol
@@ -40,19 +40,33 @@ interface IGame {
     event PlayerAdded(address indexed player, uint8 playerId);
 
     function MAX_PLAYERS() external pure returns (uint8);
+
     function totalPlayers() external view returns (uint8);
+
     function gameStatus() external view returns (GameStatus);
+
     function roundNumber() external view returns (uint256);
+
     function grid(uint8 x, uint8 y) external view returns (Cell memory);
+
     function idToAddress(uint8 id) external view returns (address);
+
     function addressToId(address player) external view returns (uint8);
-    
+
     function addPlayer() external;
+
     function makeMove(Move memory _move) external;
+
     function makeLoan(Loan memory _loan) external;
+
     function executeRound() external;
+
     function getGrid() external view returns (Cell[9] memory cells);
+
     function get2dGrid() external view returns (Cell[3][3] memory cells);
+
     function checkValidMove(Move memory _move) external view returns (bool);
+
     function checkValidLoan(Loan memory _loan) external view returns (bool);
-} 
+}
+
